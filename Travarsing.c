bdecaef86bf070c066154c0c617917a52edbaa84
Md. Alamin <alaminrakib4864@gmail.com>
2022-11-28 05:15:57 +0600

    traversing array

--- Travarsing.c
+++ Travarsing.c
@@ -13,7 +13,7 @@ int main(){
     int k;
     int LA[n] = {23, 45, 56, 1, -9, -12, 123};
     k=0; 
-    printf("Traversing the LA elements: ");
+    printf("Traversing in the LA elements: ");
     while(k<=n-1){ //-1 is for counting from 0;
         printf("%d ", LA[k]);
         k++;
