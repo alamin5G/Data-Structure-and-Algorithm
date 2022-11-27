582702f5b6a81ba10483a92fd5701fd17a6d2cd0
Md. Alamin <alaminrakib4864@gmail.com>
2022-11-28 05:07:09 +0600

    Traversing Algorithm Program

--- Travarsing.c
+++ Travarsing.c
@@ -1,7 +1,16 @@
+/**
+ * Algorithm for Traversing Element in a Linear Array.
+1. Set K = LB;
+2. Repeat Step 3 and 4 while K<= UB;
+3. 		Apply Process to LA[K];
+4. 		Set K = K+1;
+	[END OF STEP 2 LOOP]
+5. Exit;
+*/
 #include <stdio.h>
 #define n 7
 int main(){
-    int k, i;
+    int k;
     int LA[n] = {23, 45, 56, 1, -9, -12, 123};
     k=0; 
     printf("Traversing the LA elements: ");
