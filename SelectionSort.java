f51686736e491caeddd49f7f47378250bac26bca
alamin5g <21303134@iubat.edu>
2024-02-21 05:35:05 +0600

    Selection Sort - smallest to largest

---
+++ SelectionSort.java
@@ -0,0 +1,38 @@
+public class SelectionSort {
+    
+    public static void main(String[] args) {
+        int[] arr = {1,5, 9, 2, 0, 16, 58, 13, 39, 17};
+        System.out.print("Before Sorted: (Unsorted):");
+        for (int i : arr) {
+            System.out.print(" " + i);
+        }
+
+        System.out.println();
+        System.out.print("After sorted: (selection sort - smallest to large) :");
+        int[] sorted = selectionSort(arr);
+        for (int i : sorted) {
+            System.out.print(" " + i);
+        }
+
+    }
+
+    public static int[] selectionSort(int[] arr){
+
+        int smallestIndex = 0;
+
+        for (int i = 0; i < arr.length-1; i++) {
+
+            for (int j = i+1; j < arr.length; j++) {
+
+                if ( arr[j] < arr[i] ) { //arr[i] is larger than the current arr[j] postion
+                    int larger = arr[i]; 
+                    arr[i] = arr[j]; //smallest item placed into the iTH postion
+                    arr[j] = larger; //larger item placed into the current jTH position
+                }
+            }
+        }
+
+        
+        return arr;
+    }
+}
