cab242a12e32570249751cf3b5022b8da2d3142a
alamin5g <21303134@iubat.edu>
2024-03-07 00:03:19 +0600

    test

--- test.java
+++ test.java
@@ -1,45 +1,31 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class test {
-    public static void main(String[] args) {
-        int[] arr = { 7, 5, 9, 0, 4, 3, 2, 8};
-        System.out.println("Before quick sort: " + Arrays.toString(arr) );
-        quickSort(arr, 0, arr.length-1);
-        System.out.println("after quick sort: " + Arrays.toString(arr) );
-
-    }
-
-    public static void quickSort(int[] arr, int low, int high){
-
-        if (low < high){
-            int pivotIndex = partition(arr, low, high);
-            quickSort(arr, low, pivotIndex-1);
-            quickSort(arr, pivotIndex+1, high);
-        }
-    }
-
-    public static int partition(int[] arr, int low, int high){
-        int pivot = arr[high];
-        int i = low-1;
-        
-        for(int j = low; j<high; j++){
-            if (arr[j] < pivot) {
-                i++;
-                if (i == j) {
-                    continue;
-                }
-                swap(arr, i, j);
+    public static void main (String[] args)
+	{
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        //String[] res = new String[n];
+        for( int i =0;i<n;i++){
+          String s =sc.nextLine();
+            if(s.length()%2!=0){
+               //res[i]="NO";
+               System.out.println("NO");
+            }else{
+               StringBuffer sb = new StringBuffer(s);
+               if(s.endsWith(sb.subSequence(0,s.length()/2).toString())){
+                  // res[i]="YES";
+                  System.out.println("YES");
+               }else{
+                System.out.println("NO");
+                //res[i]="NO";
+               }
+                   
             }
-
         }
-        swap(arr, i+1, high);
-
-        return i + 1;
-    }
-
-    public static void swap(int[] arr, int i, int j){
-        int t = arr[i];
-        arr[i] = arr[j];
-        arr[j] = t;
+    //    for(String s :res){
+    //        System.out.println(s);
+    //    }
     }
 }
