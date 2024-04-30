24bc7c8966b2dd16b42b2636a3454065f85817e8
alamin5g <21303134@iubat.edu>
2024-04-30 08:23:03 +0600

    initialized the listNode

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -18,4 +18,18 @@ public class SinglyLinkedList {
 
     }
 
+    public static void main(String[] args) {
+        SinglyLinkedList linkedList = new SinglyLinkedList();
+        linkedList.head = new ListNode<Integer>(10);
+        ListNode secListNode = new ListNode<Integer>(1);
+        ListNode thirdListNode = new ListNode<Integer>(8);
+        ListNode fourthListNode = new ListNode<Integer>(11);
+
+        // now we will connect them togethher to form a chain
+        linkedList.head.next = secListNode; //10 --> 1;
+        secListNode.next = thirdListNode; // 10 --> 1 --> 8;
+        thirdListNode.next = fourthListNode; // 10 --> 1 --> 8 --> 11;
+
+    }
+
 }
