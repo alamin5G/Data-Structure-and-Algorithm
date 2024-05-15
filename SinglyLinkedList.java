c5d63027d839249d4cdcc354d38e9fb65b4b556a
alamin5g <alaminrakib4864@gmail.com>
2024-05-15 14:10:23 +0600

    implement the insertAt(*) and deleteAt(*) feature

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -3,8 +3,8 @@
  */
 public class SinglyLinkedList<E> {
 
-    private Node head;
-    private Node tail;
+    private Node<E> head;
+    private Node<E> tail;
     private int size;
 
     SinglyLinkedList(){
@@ -16,7 +16,7 @@ public class SinglyLinkedList<E> {
     private static class Node<E> {
     
         private E data;
-        public Node next;
+        public Node<E> next;
 
         public Node(E data){
             this.data = data;
@@ -26,7 +26,7 @@ public class SinglyLinkedList<E> {
     
     //insert linked list data from the first
     public void addFirst(E data){
-        Node newNode = new Node<E>(data);
+        Node<E> newNode = new Node<E>(data);
         if (head == null) {
             head = newNode;
             tail = newNode;
@@ -40,7 +40,7 @@ public class SinglyLinkedList<E> {
    
      //insert linked list data into the last
     public void add(E data){
-        Node newNode = new Node<E>(data);
+        Node<E> newNode = new Node<E>(data);
         if (head==null) {
             head = newNode;
             tail = newNode;
@@ -51,19 +51,108 @@ public class SinglyLinkedList<E> {
             size++;
         }
     }
+
+    //delete from the first
+    public void removeFirst(){
+        if (head == null) {
+            System.out.println("Empty list");
+        }else if(head.next == null){
+            head = null;
+            tail = null;
+            size--;
+        }else{
+            head = head.next;
+            size--;
+        }
+    }
+
+    //delete from the last
+    public void removeLast(){
+        if (head == null) {
+            System.out.println("Empty list");
+        }else if(head.next == null){
+            tail = null;
+            head = null;
+            size--;
+        }else {
+            Node<E> temp = head;
+            while (temp.next.next != null) {
+                temp = temp.next;
+            }
+            temp.next = null;
+            tail = temp;
+            size--;
+        }
+    }
      
     //to display linkedList data
     public void showData(){
         if (head == null) {
             System.out.println("Empty List");
         }else {
-            Node temp = head;
+            Node<E> temp = head;
             while (temp != null) {
                 System.out.print( temp.data + "->");
                 temp = temp.next;
             }
             System.out.println("null");
         }
+    }
+
+    //insert data at given position
+    public void insertAt(int position, E data){
+        
+        if (head==null) {
+            System.out.println("empty list");
+            return;
+        }
+
+        Node<E> newNode = new Node<E>(data);
+
+        if (position == 0 ) {
+            newNode.next = head;
+            head = newNode;
+            size++;
+        }else if(position == size){
+            tail.next = newNode;
+            tail = newNode;
+            size++;
+        }else{
+            Node<E> temp = head;
+            int count = 0;
+            while (count < position-1) {
+                count++;
+                temp = temp.next;
+            }
+            newNode.next = temp.next;
+            temp.next = newNode;
+            size++;
+        }
+    }
+
+    //delete from given position
+    public void deleteAt(int position){
+        if (head==null) {
+            System.out.println("Empty List");
+            return;
+        }
+        
+        if (position == 0 && head.next == null) {
+            head = null;
+            tail = null;
+        }else if(position == 0){
+            head = head.next;
+        }else{
+            Node temp = head;
+            int count = 0;
+            while (count < position-1) {
+                count++;
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+
+        }
+        size--;
     }
 
     // to calculate the current length of the linkedlist
@@ -83,10 +172,37 @@ public class SinglyLinkedList<E> {
         System.out.println("Size of the linked list is " + linkedList.getLength());;
         System.out.println("After adding at last...");
         linkedList.add(13);//15->4->12->1->13->1null
-        linkedList.add(4);//15->4->12->1->13->4->1null
+        linkedList.add(7);//15->4->12->1->13->4->1null
         linkedList.showData();
         System.out.println("Size of the linked list is " + linkedList.getLength());;
-        
+        System.out.println("After removing the first element from the list");
+        linkedList.removeFirst();
+        linkedList.showData();
+        System.out.println("Size of the linked list is " + linkedList.getLength());;
+        System.out.println("After removing the last element from the list");
+        linkedList.removeLast();
+        linkedList.showData();
+        System.out.println("Size of the linked list is " + linkedList.getLength());;
+        System.out.println("After adding at 0 position");
+        linkedList.insertAt(0, 10);
+        linkedList.showData();
+        System.out.println("Size = " + linkedList.getLength());
+        System.out.println("After adding at last position");
+        linkedList.insertAt(linkedList.getLength(), 20);
+        linkedList.showData();
+        System.out.println("Size = " + linkedList.getLength());
+        System.out.println("After adding at somewhere(3)");
+        linkedList.insertAt(3, 11);
+        linkedList.showData();
+        System.out.println("Size = " + linkedList.getLength());
+        System.out.println("After deleteing at somewhere(0)");
+        linkedList.deleteAt(0);
+        linkedList.showData();
+        System.out.println("Size = " + linkedList.getLength());
+        System.out.println("After deleteing at somewhere(3)");
+        linkedList.deleteAt(3);
+        linkedList.showData();
+        System.out.println("Size = " + linkedList.getLength());
 
     }
 
