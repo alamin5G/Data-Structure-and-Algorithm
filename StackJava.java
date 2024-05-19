b6996b87fa7aca430e40a4b36f7cf00b418b14a6
alamin5g <alaminrakib4864@gmail.com>
2024-05-19 17:00:13 +0600

    stack updated

--- StackJava.java
+++ StackJava.java
@@ -56,10 +56,35 @@ public class StackJava<E> {
     }
 
     //peak
-    
+    public E peak(){
+        if (top==null) {
+            return (E) "empty stack";
+        }else{ 
+        return top.data;
+        }
+    }
+
+
     //main method
     public static void main(String[] args) {
         StackJava<Integer> stack = new StackJava<>();
+        System.out.println("before push");
+        System.out.println("top = " + stack.top);
+        System.out.println("length = " + stack.length());
+        System.out.println("peak = " + stack.peak());
+        System.out.println("after push");
+        stack.push(4);
+        stack.push(3);
+        stack.push(2);
+        stack.push(1);
+        stack.push(-1);
+        System.out.println("top = " + stack.top);
+        System.out.println("length = " + stack.length());
+        System.out.println("peak = " + stack.peak());
+        System.out.println("1st pop : " + stack.pop());
+        System.out.println("2nd pop : " + stack.pop());
+        System.out.println("3nd pop : " + stack.pop());
+
 
     }
     
